97faad3447459b2fb58908fc494cf457bea2f1f1
Mark T <aydenthos@gmail.com>
2023-10-20 06:59:02 -0700

    routes

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
+const routes = require('./routes');
 
 // const bodyParser = require('body-parser');
 
@@ -26,6 +27,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
+app.use(routes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/dist/index.html'));
