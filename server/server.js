d7ac2bb795aa4c03b3afd176a7c8da5d3d5316fc
Mark T <aydenthos@gmail.com>
2023-10-19 22:20:53 -0700

    unhooked the api

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+
 // const bodyParser = require('body-parser');
 
 const { typeDefs, resolvers } = require('./schemas');
@@ -26,11 +26,12 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes);
+
 
 const startApolloServer = async () => {
   await apolloServer.start();
